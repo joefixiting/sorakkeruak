17d35b8729309ed714b72511ef3c70a6d15eef50
joefixiting <muhammadrafifahnafefendi@mail.ugm.ac.id>
2025-08-04 23:10:57 +0800

    fix: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import IconKKN from '../assets/icons/kkn-ppm.svg';
 
 const Header = () => {
   return (
-    <header className="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex justify-between items-center py-4">
+    <header className="relative z-10 mx-auto max-w-1xl px-4 sm:px-6 lg:px-8 flex justify-between items-center py-4">
       <div className="flex items-center gap-2">
         {/* NOTE: In a real app, you'd use <img/> tags with the imported assets */}
         <img className="h-10 w-10 object-contain" alt="Organization logo" src={IconUGM} />
